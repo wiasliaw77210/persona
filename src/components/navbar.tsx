5bae3b7aed34ea6135f0222b376e81c6b8867a33
Wei Lee <leewei48750@gmail.com>
2019-06-23 02:39:24 +0800

    feat(src/): navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,6 @@
 import * as React from 'react';
-import styledComponents from 'styled-components';
 
-const FlexDiv = styledComponents.div`
-  displayL flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-
-export class Navbar extends React.Component {
+export class Navbar extends React.Component<{}, {}> {
   render() {
     return (
       <nav id="navbar">
@@ -16,9 +8,9 @@ export class Navbar extends React.Component {
           <a className="navbar-link" href="">About</a>
           <a className="navbar-link" href="">Snippets</a>
         </div>
-        <FlexDiv>
+        <div>
           <a className="navbar-link" href="">Contact</a>
-        </FlexDiv>
+        </div>
       </nav>
     );
   }
