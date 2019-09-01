6067236f04040cb0d7276dcaca2d42a53805469a
Wei Lee <leewei48750@gmail.com>
2019-09-02 02:39:25 +0800

    feat(src/): contact.tsx: add main content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,11 +4,9 @@ import Container from './container/Container';
 export default class About extends React.Component {
   render() {
     return (
-      <div>
-        <Container>
-          <h1>About Text</h1>
-        </Container>
-      </div>
+      <Container>
+        <h1>About Text</h1>
+      </Container>
     );
   }
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,66 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTelegram,
+  faGithub,
+  faDiscord,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Container from './container/Container';
+import Wraper from './container/Wraper';
+import Floatdiv from './container/Floatdiv';
+import incoJpg from '../../static/img/inco.jpg';
 
 export default class Contact extends React.Component {
   render() {
+    const styles = {
+      padding: '5%',
+      display: 'flex',
+      'flex-direction': 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
     return (
       <Container>
-        <h1>Contact Text</h1>
+        <Wraper width="800px" height="600px">
+          <Floatdiv class="float-div" float="left">
+            <div style={styles}>
+              <img className="contact-img" src={incoJpg} alt="icon"/>
+            </div>
+            <div style={styles}>
+              <h2>Wias Liaw</h2>
+              <hr/>
+            </div>
+          </Floatdiv>
+          <Floatdiv class="float-div" float="right">
+            <a className="contact" href="mailto:leewei48750@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+              <span>leewei48750@gmail.com</span>
+            </a>
+            <a className="contact" target="_blank">
+              <FontAwesomeIcon icon={faFacebook} className="contact-icon" />
+              <span>Facebook</span>
+            </a>
+            <a className="contact" target="_blank" href="https://t.me/chialiaw">
+              <FontAwesomeIcon icon={faTelegram} className="contact-icon" />
+              <span>Telegram</span>
+            </a>
+            <a className="contact" target="_blank" href="https://github.com/wiasliaw77210">
+              <FontAwesomeIcon icon={faGithub} className="contact-icon" />
+              <span>wiasliaw77210</span>
+            </a>
+            <a className="contact" target="_blank" href="https://discordapp.com/">
+              <FontAwesomeIcon icon={faDiscord} className="contact-icon" />
+              <span>Discord</span>
+            </a>
+            <a className="contact" target="_blank" href="">
+              <FontAwesomeIcon icon={faLinkedin} className="contact-icon" />
+              <span>Linkedin</span>
+            </a>
+          </Floatdiv>
+        </Wraper>
       </Container>
     );
   }
